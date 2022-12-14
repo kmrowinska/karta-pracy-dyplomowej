d8aefd92f41463be8647c5b576c99cb15d4766c1
kmrowinska <kingamrowinska1@gmail.com>
2022-12-15 00:01:34 +0100

    zm2

--- karta pracy/Form1.Designer.cs
+++ karta pracy/Form1.Designer.cs
@@ -63,6 +63,8 @@
             this.textBox10 = new System.Windows.Forms.TextBox();
             this.textBox11 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -365,19 +367,30 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(695, 704);
+            this.button1.Location = new System.Drawing.Point(662, 791);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(109, 29);
             this.button1.TabIndex = 33;
-            this.button1.Text = "zapisz";
+            this.button1.Text = "zapis do .peg";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(662, 826);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 29);
+            this.button2.TabIndex = 34;
+            this.button2.Text = ".peg na .pdf";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(811, 745);
+            this.ClientSize = new System.Drawing.Size(793, 856);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox11);
             this.Controls.Add(this.textBox10);
@@ -456,5 +469,7 @@
         private TextBox textBox10;
         private TextBox textBox11;
         private Button button1;
+        private Button button2;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
--- karta pracy/Form1.cs
+++ karta pracy/Form1.cs
@@ -1,9 +1,17 @@
+using ArrayToPdf;
 using iTextSharp.text;
+using iTextSharp.text.pdf;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.Data;
+using System.Windows.Forms;
+using Image = iTextSharp.text.Image;
 
 namespace karta_pracy
 {
     public partial class Form1 : Form
     {
+        public List<Form1> dane = new List<Form1>();
         public Form1()
         {
             InitializeComponent();
@@ -112,26 +120,41 @@ namespace karta_pracy
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using(SaveFileDialog save = new SaveFileDialog() { Filter = "PDF file|*.pdf", ValidateNames= true })
+           MessageBox.Show("Przed zapisem");
+            var doc = Form.ActiveForm;
+            using(var bmp = new Bitmap(doc.Width, doc.Height)) 
             {
-                if(save.ShowDialog() == DialogResult.OK)
+                doc.DrawToBitmap(bmp, new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height));
+                bmp.Save(@"C:\Users\kinga\Desktop\karta.png");
+                MessageBox.Show("Udalo sie!");
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Przed zapisem");
+            /*Document doc = new Document();
+            using (var stream = new FileStream("karta.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                PdfWriter.GetInstance(doc, stream);
+                doc.Open();
+                using (var imageStream = new FileStream("karta.png", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
                 {
-                    iTextSharp.text.Document doc = new iTextSharp.text.Document(PageSize.A4.Rotate());
-                    try
-                    {
-                        iTextSharp.text.pdf.PdfWriter.GetInstance(doc, new FileStream(save.FileName, FileMode.Create));
-                        doc.Open();
-                        doc.Add(new iTextSharp.text.Paragraph());
-                    }
-                    catch(Exception ex) 
-                    {
-                        MessageBox.Show(ex.Message, "B³¹d przy zapisywaniu!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                    finally
-                    {
-                        doc.Close();
-                    }
+                    var img = Image.GetInstance(imageStream);
+                    doc.Add(img);
                 }
+                doc.Close();
+                MessageBox.Show("Udalo sie!");
+            }*/
+
+            PrintDialog dialog1 = new PrintDialog();
+            dialog1.Document = printDocument1;
+            DialogResult r = dialog1.ShowDialog();
+
+            if(r == DialogResult.OK) 
+            {
+                printDocument1.Print();
             }
         }
     }
